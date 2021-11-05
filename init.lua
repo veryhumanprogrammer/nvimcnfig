89ac9bc79d600bf49570b28c5ff099d4d5939ac8
veryhumanprogrammer <definitelyahuman0@protonmail.com>
2021-11-05 17:42:59 +0000

    Made Neovim transparent (thanks to Mia <3)

--- init.lua
+++ init.lua
@@ -36,6 +36,7 @@ require("plugins")
 	vim.o.tabstop = 2
 	vim.o.shiftwidth = 2
 	vim.o.expandtab = true
+  vim.g.tokyonight_transparent = true
 	vim.cmd("colorscheme tokyonight")
 	vim.cmd("set nu rnu")
 
